74ee6280b905a7c7daedd8679fc517bdc1cf2819
niooii <onioniwonpounoin@gmail.com>
2026-02-15 19:49:53 -0500

    add scaffolding and deps

--- .util/src/main.rs
+++ .util/src/main.rs
@@ -1,8 +1,20 @@
+use anyhow::Result;
+use deepwoken_reqparse::{req::Requirement, util::reqtree::ReqTree};
+
 fn main() {
     println!("Hello, world!");
 }
 
+/* A crate to test the data is well formed */
+
+// Contruct a new requirement tree from all named objects in the data
+fn get_tree() -> Result<ReqTree> {
+    todo!()
+}
+
 #[test]
 fn all_prereqs_exist() {
+    let mut tree = get_tree().unwrap();
+
     todo!()
 }
